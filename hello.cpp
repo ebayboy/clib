0874bedb8d6341dd3ff3c3e3f18cc704e3928475
fanpengfei1 <fanpengfei@jd.com>
2019-10-24 09:36:29 +0800

    fw

---
+++ hello.cpp
@@ -0,0 +1,24 @@
+/****************************************************************************
+@file:hello.cpp 
+@author:ebayboy@163.com 
+@date:2019-10-24 09:32 
+@version: 1.0  
+@description: cpp file 
+@Copyright (c)  all right reserved 
+**************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <numeric>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define DEBUG 
+
+void Hello()
+{
+	cout << "Hello world!" << endl;
+}
+
---
+++ main.cpp
@@ -0,0 +1,28 @@
+/****************************************************************************
+@file:main.cpp 
+@author:ebayboy@163.com 
+@date:2019-10-24 09:32 
+@version: 1.0  
+@description: cpp file 
+@Copyright (c)  all right reserved 
+**************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <numeric>
+#include <vector>
+#include <algorithm>
+
+#include "hello.h"
+
+using namespace std;
+
+#define DEBUG 
+
+int main(int argc, char **argv)
+{
+	Hello();
+
+	return 0;
+}
+
